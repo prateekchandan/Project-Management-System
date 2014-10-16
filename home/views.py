fcf65b102bbd8bf9818ceace358f828f25b62eb6
prateekchandan <prateekchandan5545@gmail.com>
2014-10-16 17:02:18 +0530

    :grin: :sweat: changing project from django to java - jsp :triumph: :sob: :trollface: /play trombone

--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
-from django.template.loader import get_template
-from home.models import *
-
-def home(request):
-	return render(request,"index.html",{"name":"Prateek"})
-	

e2aad1c25209ca32473acbf6f6851fa5ac25dc9f
prateekchandan <prateekchandan5545@gmail.com>
2014-10-30 23:20:29 +0530

    Added Project Report :wink:

--- src/project/all.java
+++ src/project/all.java
@@ -91,7 +91,7 @@ public class all extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		doGet(request,response);
 	}
 
 }

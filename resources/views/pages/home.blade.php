41a31f3e0fdd78a0d85c5cc6c9057502adef1dc4
Shyam JVS <shyam123.jvs95@gmail.com>
2016-05-02 00:09:11 +0530

    aur badal diya 3:)

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -97,8 +97,9 @@
 	 <div class="container mtb">
 	 	<div class="row">
 	 		<div class="col-lg-4 col-lg-offset-1">
-		 		<h4>More About Our Agency.</h4>
-		 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+		 		<h4>More About Our Project.</h4>
+		 		<p>Project Workflow management system is a Web-based PHP application where students, professors and alumni from a particular institute or different institutes can list up the current projects they are working on or have previously worked upon or planning to work in the future.
+		 		</p>
  				<p><br/><a href="about.html" class="btn btn-theme">More Info</a></p>
 	 		</div>
 	 		
